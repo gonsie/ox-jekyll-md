a530cfeb79230547f4ef7c03fc94390a476f7ede
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2018-08-31 10:31:09 -0700

    change to jekyll-md to prevent name conflicts
    IDK how emacs namespacing works? I guess it doesn't.

--- ox-jekyll-md.el
+++ ox-jekyll-md.el
@@ -1,4 +1,4 @@
-;;; ox-jekyll.el --- Export Jekyll articles using org-mode. -*- lexical-binding: t; -*-
+;;; ox-jekyll-md.el --- Export Jekyll on Markdown articles using org-mode. -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2018 Elsa Gonsiorowski
 
@@ -27,11 +27,11 @@
 ;; Org exporter, based on `md' back-end.
 ;;
 ;; It provides two commands for export, depending on the desired
-;; output: `org-jekyll-export-as-md' (temporary buffer) and
-;; `org-jekyll-export-to-md' ("md" file with YAML front matter).
+;; output: `org-jekyll-md-export-as-md' (temporary buffer) and
+;; `org-jekyll-md-export-to-md' ("md" file with YAML front matter).
 ;;
-;; For publishing, `org-jekyll-publish-to-md' is available.
-;; For composing, `org-jekyll-insert-export-options-template' is available.
+;; For publishing, `org-jekyll-md-publish-to-md' is available.
+;; For composing, `org-jekyll-md-insert-export-options-template' is available.
 
 ;;; Code:
 
@@ -47,7 +47,7 @@
   :group 'org-export
   :version "24.2")
 
-(defcustom org-jekyll-include-yaml-front-matter t
+(defcustom org-jekyll-md-include-yaml-front-matter t
   "If true, then include yaml-front-matter when exporting to md.
 
 If false, then you should include the yaml front matter like this at the top of
@@ -68,23 +68,23 @@ description: Instructions on Upgrading Octopress
   :type 'boolean)
 
 
-(defcustom org-jekyll-layout "post"
+(defcustom org-jekyll-md-layout "post"
   "Default layout used in Jekyll article."
   :group 'org-export-jekyll
   :type 'string)
 
-(defcustom org-jekyll-categories ""
+(defcustom org-jekyll-md-categories ""
   "Default space-separated categories in Jekyll article."
   :group 'org-export-jekyll
   :type 'string)
 
-(defcustom org-jekyll-tags ""
+(defcustom org-jekyll-md-tags ""
   "Default space-separated tags in Jekyll article."
   :group 'org-export-jekyll
   :type 'string)
 
-(defcustom org-jekyll-use-src-plugin t
-   "If t, org-jekyll exporter eagerly uses plugins instead of
+(defcustom org-jekyll-md-use-src-plugin t
+   "If t, org-jekyll-md exporter eagerly uses plugins instead of
 original markdown stuff. For example:
 
    #+BEGIN_SRC ruby
@@ -99,38 +99,38 @@ makes:
   :group 'org-export-jekyll-use-src-plugin
   :type 'boolean)
 
-(defcustom org-jekyll-use-todays-date t
-  "If t, org-jekyll exporter will prepend the filename with today's date."
+(defcustom org-jekyll-md-use-todays-date t
+  "If t, org-jekyll-md exporter will prepend the filename with today's date."
   :group 'org-export-jekyll
   :type 'boolean)
 
 ;;; Define Back-End
 
 (org-export-define-derived-backend 'jekyll 'md
-  :filters-alist '((:filter-parse-tree . org-jekyll-separate-elements))
+  :filters-alist '((:filter-parse-tree . org-jekyll-md-separate-elements))
   :menu-entry
   '(?j "Jekyll: export to Markdown with YAML front matter."
-       ((?M "As Jekyll buffer" (lambda (a s v b) (org-jekyll-export-as-md a s v)))
-        (?m "As Jekyll file" (lambda (a s v b) (org-jekyll-export-to-md a s v)))))
+       ((?M "As Jekyll buffer" (lambda (a s v b) (org-jekyll-md-export-as-md a s v)))
+        (?m "As Jekyll file" (lambda (a s v b) (org-jekyll-md-export-to-md a s v)))))
   :translate-alist
-  '((headline . org-jekyll-headline-offset)
-    (inner-template . org-jekyll-inner-template) ;; force body-only
-    (src-block . org-jekyll-src-block)
-    (table . org-jekyll-table)
-    (table-cell . org-jekyll-table-cell)
-    (table-row . org-jekyll-table-row)
-    (template . org-jekyll-template)) ;; add YAML front matter.
+  '((headline . org-jekyll-md-headline-offset)
+    (inner-template . org-jekyll-md-inner-template) ;; force body-only
+    (src-block . org-jekyll-md-src-block)
+    (table . org-jekyll-md-table)
+    (table-cell . org-jekyll-md-table-cell)
+    (table-row . org-jekyll-md-table-row)
+    (template . org-jekyll-md-template)) ;; add YAML front matter.
   :options-alist
-  '((:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-layout)
-    (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-categories)
-    (:jekyll-tags "JEKYLL_TAGS" nil org-jekyll-tags)))
+  '((:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-md-layout)
+    (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-md-categories)
+    (:jekyll-tags "JEKYLL_TAGS" nil org-jekyll-md-tags)))
 
 
 ;;; Headline
 ;;; Keep the level as defined in original content
 ;;; ** subtree => ## heading
 
-(defun org-jekyll-headline-offset (headline contents info)
+(defun org-jekyll-md-headline-offset (headline contents info)
   "proper headline offset"
   (let* ((info (plist-put info :headline-offset 0)))
     (org-md-headline headline contents info)))
@@ -138,7 +138,7 @@ makes:
 
 ;;; Internal Filters
 
-(defun org-jekyll-separate-elements (tree _backend info)
+(defun org-jekyll-md-separate-elements (tree _backend info)
   "Fix blank lines between elements.
 
 TREE is the parse tree being exported.  BACKEND is the export
@@ -173,13 +173,13 @@ Assume BACKEND is `jekyll'."
   ;; Return updated tree.
   tree)
 
-(defun org-jekyll-src-block (src-block contents info)
+(defun org-jekyll-md-src-block (src-block contents info)
   "Optionally transcode SRC-BLOCK element into jekyll code template format.
 
-Use `highlight` / `endhighlight` if `org-jekyll-use-src-plugin` is t. Otherwise,
+Use `highlight` / `endhighlight` if `org-jekyll-md-use-src-plugin` is t. Otherwise,
 perform `org-md-src-block`. CONTENTS holds the contents of the item. INFO is a
 plist used as a communication channel."
-  (if org-jekyll-use-src-plugin
+  (if org-jekyll-md-use-src-plugin
       (let ((language (org-element-property :language src-block))
             (value (org-remove-indentation
                     (org-element-property :value src-block))))
@@ -187,17 +187,17 @@ plist used as a communication channel."
                 language value))
     (org-export-with-backend 'md src-block contents info)))
 
-(defun org-jekyll-table (table contents info)
+(defun org-jekyll-md-table (table contents info)
   "Empty transformation. Org tables should be valid kramdown syntax."
   contents)
 
-(defun org-jekyll-table-cell (table-cell contents info)
+(defun org-jekyll-md-table-cell (table-cell contents info)
   "Empty transformation. Org tables should be valid kramdown syntax."
   (if contents
       (format "| %s " contents)
     "| "))
 
-(defun org-jekyll-table-row (table-row contents info)
+(defun org-jekyll-md-table-row (table-row contents info)
   "Empty transformation. Org tables should be valid kramdown syntax."
   (if contents
       (format "%s|" contents)
@@ -208,19 +208,19 @@ plist used as a communication channel."
 
 ;;; Template
 
-(defun org-jekyll-template (contents info)
+(defun org-jekyll-md-template (contents info)
   "Return complete document string after MD conversion.
 
 CONTENTS is the transcoded contents string. INFO is a plist
 holding export options."
-  (if org-jekyll-include-yaml-front-matter
+  (if org-jekyll-md-include-yaml-front-matter
       (concat
-       (org-jekyll--yaml-front-matter info)
+       (org-jekyll-md--yaml-front-matter info)
        contents)
     contents
     ))
 
-(defun org-jekyll-inner-template (contents info)
+(defun org-jekyll-md-inner-template (contents info)
   "Return body of document string after MD conversion.
 
 CONTENTS is the transcoded contents string.  INFO is a plist
@@ -237,12 +237,12 @@ holding export options."
 
 ;;; YAML Front Matter
 
-(defun org-jekyll--get-option (info property-name &optional default)
+(defun org-jekyll-md--get-option (info property-name &optional default)
   "Get org export options."
   (let ((property (org-export-data (plist-get info property-name) info)))
     (format "%s" (or property default ""))))
 
-(defun org-jekyll--yaml-front-matter (info)
+(defun org-jekyll-md--yaml-front-matter (info)
   "Creat YAML frontmatter content."
   (let ((convert-to-yaml-list
          (lambda (arg)
@@ -250,28 +250,28 @@ holding export options."
                       (split-string arg) " "))))
     (let ((title
            (concat "\ntitle: \""
-                   (org-jekyll--get-option info
+                   (org-jekyll-md--get-option info
                                            :title) "\""))
           (layout
            (concat "\nlayout: "
-                   (org-jekyll--get-option info
-                                           :jekyll-layout org-jekyll-layout)))
+                   (org-jekyll-md--get-option info
+                                           :jekyll-layout org-jekyll-md-layout)))
           (categories
            (concat "\ncategories: "
                    (funcall convert-to-yaml-list
-                            (org-jekyll--get-option
+                            (org-jekyll-md--get-option
                              info
-                             :jekyll-categories org-jekyll-categories))))
+                             :jekyll-categories org-jekyll-md-categories))))
           (tags
            (concat "\ntags: "
                    (funcall convert-to-yaml-list
-                            (org-jekyll--get-option
+                            (org-jekyll-md--get-option
                              info
-                             :jekyll-tags org-jekyll-tags))))
+                             :jekyll-tags org-jekyll-md-tags))))
           (date
            (and (plist-get info :with-date)
                 (concat "\ndate: "
-                        (org-jekyll--get-option info :date)))))
+                        (org-jekyll-md--get-option info :date)))))
       (concat
        "---"
        title
@@ -285,9 +285,9 @@ holding export options."
 ;;; Filename and Date Helper
 ;;; optionally prepend filename with today's date
 
-(defun org-jekyll-filename-date ()
+(defun org-jekyll-md-filename-date ()
   "Optionally include date in exported filename."
-  (if org-jekyll-use-todays-date
+  (if org-jekyll-md-use-todays-date
       (format-time-string "%F-")
     ""))
 
@@ -295,22 +295,22 @@ holding export options."
 ;;; End-User functions
 
 ;;;###autoload
-(defun org-jekyll-export-as-md (&optional async subtreep visible-only)
+(defun org-jekyll-md-export-as-md (&optional async subtreep visible-only)
   "Export current buffer as a Markdown buffer adding some YAML front matter."
   (interactive)
   (org-export-to-buffer 'jekyll "*Org Jekyll-Markdown Export*"
     async subtreep visible-only nil nil (lambda () (text-mode))))
 
 ;;;###autoload
-(defun org-jekyll-export-to-md (&optional async subtreep visible-only)
+(defun org-jekyll-md-export-to-md (&optional async subtreep visible-only)
   "Export current buffer to a Markdown file adding some YAML front matter."
   (interactive)
-  (let ((outfile (concat (org-jekyll-filename-date)
+  (let ((outfile (concat (org-jekyll-md-filename-date)
                          (org-export-output-file-name ".md" subtreep))))
     (org-export-to-file 'jekyll outfile async subtreep visible-only)))
 
 ;;;###autoload
-(defun org-jekyll-publish-to-md (plist filename pub-dir)
+(defun org-jekyll-md-publish-to-md (plist filename pub-dir)
   "Publish an org file to Markdown with YAML front matter.
 
 FILENAME is the filename of the Org file to be published.  PLIST
@@ -321,13 +321,13 @@ Return output file name."
   (org-publish-org-to 'jekyll filename ".md" plist pub-dir))
 
 ;;;###autoload
-(defun org-jekyll-insert-export-options-template
+(defun org-jekyll-md-insert-export-options-template
   (&optional title date setupfile categories tags layout)
   "Insert a settings template for Jekyll exporter."
   (interactive)
-  (let ((layout     (or layout org-jekyll-layout))
-        (tags       (or tags org-jekyll-tags))
-        (categories (or categories org-jekyll-categories)))
+  (let ((layout     (or layout org-jekyll-md-layout))
+        (tags       (or tags org-jekyll-md-tags))
+        (categories (or categories org-jekyll-md-categories)))
     (save-excursion
       (insert (format (concat
                        "#+TITLE: "             title
@@ -340,6 +340,6 @@ Return output file name."
 
 ;;; provide
 
-(provide 'ox-jekyll)
+(provide 'ox-jekyll-md)
 
-;;; ox-jekyll.el ends here
+;;; ox-jekyll-md.el ends here
